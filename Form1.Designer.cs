ea75b9cebdf948ef101a181fefdb744f27a9a32d
nikitagladskih <nikitagladskih@mail.ru>
2024-08-12 10:22:03 +0500

    test
    test

---
+++ Form1.Designer.cs
@@ -0,0 +1,221 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.LightSkyBlue;
+            resources.ApplyResources(this.pictureBox1, "pictureBox1");
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            resources.ApplyResources(this.button1, "button1");
+            this.button1.Name = "button1";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // label2
+            // 
+            this.label2.BackColor = System.Drawing.Color.Silver;
+            resources.ApplyResources(this.label2, "label2");
+            this.label2.Name = "label2";
+            // 
+            // label3
+            // 
+            this.label3.BackColor = System.Drawing.Color.Silver;
+            resources.ApplyResources(this.label3, "label3");
+            this.label3.Name = "label3";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            resources.ApplyResources(this.listBox1, "listBox1");
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Click += new System.EventHandler(this.listBox1_CleClick);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            resources.ApplyResources(this.button2, "button2");
+            this.button2.Name = "button2";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label8
+            // 
+            this.label8.BackColor = System.Drawing.Color.Silver;
+            resources.ApplyResources(this.label8, "label8");
+            this.label8.Name = "label8";
+            // 
+            // label7
+            // 
+            resources.ApplyResources(this.label7, "label7");
+            this.label7.Name = "label7";
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            resources.ApplyResources(this.listBox2, "listBox2");
+            this.listBox2.Name = "listBox2";
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.Color.LightSkyBlue;
+            resources.ApplyResources(this.pictureBox2, "pictureBox2");
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.Color.Silver;
+            resources.ApplyResources(this.label1, "label1");
+            this.label1.Name = "label1";
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            resources.ApplyResources(this.button3, "button3");
+            this.button3.Name = "button3";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            resources.ApplyResources(this.button4, "button4");
+            this.button4.Name = "button4";
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // label10
+            // 
+            resources.ApplyResources(this.label10, "label10");
+            this.label10.Name = "label10";
+            // 
+            // label4
+            // 
+            resources.ApplyResources(this.label4, "label4");
+            this.label4.Name = "label4";
+            // 
+            // label5
+            // 
+            resources.ApplyResources(this.label5, "label5");
+            this.label5.Name = "label5";
+            // 
+            // label6
+            // 
+            resources.ApplyResources(this.label6, "label6");
+            this.label6.Name = "label6";
+            // 
+            // label9
+            // 
+            resources.ApplyResources(this.label9, "label9");
+            this.label9.Name = "label9";
+            // 
+            // Form1
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label9;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,244 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        string[] linesLic;
+        string nameCheckFile;
+        string nameLic;
+        
+        string path = Path.Combine(Environment.CurrentDirectory, "Log.txt");
+        
+        public Form1()
+        {
+            try
+            {
+                InitializeComponent();
+                listBox1.Click += new EventHandler(listBox1_CleClick);
+                
+            }
+            catch (Exception ex)
+            {
+                File.AppendAllText(path, "\n"+ex.Message);
+                
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        private void button4_Click(object sender, EventArgs e)
+        {
+            try {
+                if (nameCheckFile.Count() != 0)
+                {
+                    listBox2.Items.Clear();
+                    label5.Text = "";
+                    label6.Text = "";
+                    string file = File.ReadAllText(nameCheckFile);
+                    string error = fileBracketCheck(file);
+                    if (error.Length != 0)
+                    {
+                        label5.Text = "Не пройдена";
+                        listBox2.Items.Add(error);
+                    }
+                    else
+                    {
+                        label5.Text = "Пройдена успешно";
+                    }
+                    if (fileLiteralCheck(file))
+                    {
+                        label6.Text = "Не пройдена";                        
+                    }
+                    else
+                    {
+                        label6.Text = "Пройдена успешно";
+                    }
+                    
+                }
+                else
+                {
+                    listBox2.Items.Add("Не выбран файл");
+                }
+            }
+            catch (Exception ex)
+            {
+                File.AppendAllText(path, "\n" + ex.Message);
+
+            }
+
+        }
+
+        private bool fileLiteralCheck(string str)
+        {
+            try
+            {
+                bool errors = false;
+                Regex regex = new Regex(@"(\s@\s)|(\s'\s)|(\s""\s)|(\s\\\s)|(\s/\s)|(^@\s)|(^'\s)|(^""\s)|(^\\\s)|(^/\s)|(^@$)|(^'$)|(^""$)|(^\\$)|(^/$)|(\s@$)|(\s'$)|(\s""$)|(\s\\$)|(\s/$)");
+                MatchCollection matches = regex.Matches(str);
+                if (matches.Count > 0)
+                {
+                    errors = true;
+                }
+                return errors;
+            }
+            catch (Exception ex)
+            {
+                File.AppendAllText(path, "\n" + ex.Message);
+                bool errors = true;
+                return errors;
+            }
+
+        }
+
+
+        private string fileBracketCheck(string str)
+        {
+            try {
+                string errors = "";
+                int line = 1;
+                var stack = new Stack<char>();
+                var lineErrorStack = new Stack<int>();
+                foreach (var c in str)
+                {
+                    char i = 'q';
+                    switch (c)
+                    {
+                        case '\n':
+                            line++;
+                            break;
+                        case '{':
+                        case '(':
+                        case '[':
+                            stack.Push(c);
+                            lineErrorStack.Push(line);
+                            break;
+
+                        case '}':
+                            if (stack.Count == 0) { return errors = ("В стр. " + line + " незакрытая }"); };
+                            i = stack.Pop();
+                            if (i != '{') { return errors = ("В стр. " + lineErrorStack.Pop() + " незакрытая " + i); };
+                            lineErrorStack.Pop();
+                            break;
+                        case ']':
+                            if (stack.Count == 0) { return errors = ("В стр. " + line + " незакрытая ]"); };
+                            i = stack.Pop();
+                            if (i != '[') { return errors = ("В стр. " + lineErrorStack.Pop() + " незакрытая " + i); };
+                            lineErrorStack.Pop();
+                            break;
+                        case ')':
+                            if (stack.Count == 0) { return errors = ("В стр. " + line + " незакрытая )"); };
+                            i = stack.Pop();
+                            if (i != '(') { return errors = ("В стр. " + lineErrorStack.Pop() + " незакрытая " + i); };
+                            lineErrorStack.Pop();
+                            break;
+                    }
+
+                }
+                if (stack.Count != 0) { return errors = ("В стр. " + lineErrorStack.Pop() + " незакрытая " + stack.Pop()); };
+                return errors;
+            }
+            catch (Exception ex)
+            {
+                File.AppendAllText(path, "\n" + ex.Message);
+                string errors = "";
+                return errors;
+            }
+
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {//Добавление файла лицензии
+            try {
+                OpenFileDialog openFileDialog = new OpenFileDialog();
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    linesLic = File.ReadAllLines(openFileDialog.FileName); // читаем все строки из файла  
+                    label9.Text = openFileDialog.SafeFileName;
+                    nameLic = openFileDialog.FileName;
+                }
+            }
+            catch (Exception ex)
+            {
+                File.AppendAllText(path, "\n" + ex.Message);
+
+            }
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {//Добавление из файла лицензии в проверяемый файл
+            try {
+                if (nameLic.Count() != 0 && nameCheckFile.Count() != 0)
+                {
+                    File.WriteAllLines(nameCheckFile, linesLic.Concat(File.ReadAllLines(nameCheckFile)).ToArray());
+                }
+                else
+                {
+                    listBox2.Items.Add("Не выбран проверяемый файл или файл лицензии");
+                }
+            }
+            catch (Exception ex)
+            {
+                File.AppendAllText(path, ex.Message);
+
+            }
+
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {//Добавление Файла
+            try {
+                OpenFileDialog openFileDialog = new OpenFileDialog();
+                openFileDialog.Filter = "txt files (*.cs)|*.cs|All files (*.*)|*.*";
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    label5.Text = "";
+                    label6.Text = "";
+                    nameCheckFile = openFileDialog.FileName;
+                    label4.Text = openFileDialog.FileName;
+                    listBox1.Items.Add(nameCheckFile);
+
+                }
+            }
+            catch (Exception ex)
+            {
+                File.AppendAllText(path, "\n" + ex.Message);
+
+            }
+
+        }
+        
+        private void listBox1_CleClick(object sender, EventArgs e)
+        {
+            try {
+                if (listBox1.SelectedItem != null)
+                {
+                    label5.Text = "";
+                    label6.Text = "";
+                    nameCheckFile = listBox1.SelectedItem.ToString();
+                    label4.Text = listBox1.SelectedItem.ToString();
+                }
+            }
+            catch (Exception ex)
+            {
+                File.AppendAllText(path, "\n" + ex.Message);
+
+            }
+
+        }
+    }
+}
